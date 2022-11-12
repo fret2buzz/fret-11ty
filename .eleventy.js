58044f6cf45e511b270d7516a961e1022a4e26bf
fret2buzz <fret2buzz@hotmail.com>
2022-11-12 22:14:07 +0200

    Initial commit

--- .eleventy.js
+++ .eleventy.js
@@ -109,7 +109,7 @@ module.exports = function (config) {
     return {
         dir: {
             input: 'src',
-            output: 'docs',
+            output: 'dist',
             includes: 'includes',
             layouts: 'layouts',
         },
